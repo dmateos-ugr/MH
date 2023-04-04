c76ae008d83cce0355b9d14a3b48f044f0ebe4e5
David Mateos Romero <davidmateos@correo.ugr.es>
2023-04-05 00:10:39 +0200

    P1: busqueda local y relief
    Creo que tengo que normalizar los ejemplos segun los rangos de cada
    atributo en todas las particiones, no una a una

--- P1/src/main.zig
+++ P1/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Random = std.rand.Random;
 const Allocator = std.mem.Allocator;
 
 const Example = struct {
@@ -18,26 +19,20 @@ const Example = struct {
     }
 
     fn distance(self: Example, other: Example, _: void) f64 {
-        const n = self.attributes.len;
-        std.debug.assert(other.attributes.len == n);
         var ret: f64 = 0;
-        var i: usize = 0;
-        while (i < n) : (i += 1) {
-            const dist = self.attributes[i] - other.attributes[i];
+        for (self.attributes, other.attributes) |attr1, attr2| {
+            const dist = attr1 - attr2;
             ret += dist * dist;
         }
         return ret;
     }
 
     fn distanceWeighted(self: Example, other: Example, w: []const f64) f64 {
-        const n = self.attributes.len;
-        std.debug.assert(other.attributes.len == n and w.len == n);
         var ret: f64 = 0;
-        var i: usize = 0;
-        while (i < self.attributes.len) : (i += 1) {
-            if (w[i] < MIN_WEIGHT) continue;
-            const dist = self.attributes[i] - other.attributes[i];
-            ret += dist * dist * w[i];
+        for (self.attributes, other.attributes, w) |attr1, attr2, weight| {
+            if (weight < MIN_WEIGHT) continue;
+            const dist = attr1 - attr2;
+            ret += dist * dist * weight;
         }
         return ret;
     }
@@ -73,7 +68,7 @@ pub fn readExamplesFromFile(filename: []const u8, allocator: Allocator) ![]Examp
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const num_commas = std.mem.count(u8, line, ",");
         if (num_commas != num_attributes) {
-            print("Error reading file: invalid example '{s}' (expected {} attributes + class, found {})\n", .{line, num_attributes, num_commas});
+            print("Error reading file: invalid example '{s}' (expected {} attributes + class, found {})\n", .{ line, num_attributes, num_commas });
             return error.InvalidExample;
         }
 
@@ -81,7 +76,6 @@ pub fn readExamplesFromFile(filename: []const u8, allocator: Allocator) ![]Examp
         var i: usize = 0;
         var iter = std.mem.tokenize(u8, line, ",");
         while (iter.next()) |token| : (i += 1) {
-            // std.debug.print("token: {s}\n", .{token});
             if (i < num_attributes) {
                 const f = std.fmt.parseFloat(f64, token) catch unreachable;
                 attributes[i] = f;
@@ -102,8 +96,7 @@ pub fn readExamplesFromFile(filename: []const u8, allocator: Allocator) ![]Examp
 
 fn normalizeExamples(examples: []Example) void {
     const n = examples[0].attributes.len;
-    var i: usize = 0;
-    while (i < n) : (i += 1) {
+    for (0..n) |i| {
         // Calculate max and min of the attribute
         var min = examples[0].attributes[i];
         var max = min;
@@ -119,35 +112,43 @@ fn normalizeExamples(examples: []Example) void {
             example.attributes[i] = new_value;
         }
     }
-
 }
 
 const ALPHA = 0.8;
 
-pub fn fitness(w: []const f64) f64 {
-    return ALPHA*tasa_clas(w) + (1 - ALPHA)*tasa_red(w);
+pub fn getFitness(w: []const f64, test_set: []const Example, training_set: []const Example) f64 {
+    return ALPHA * tasaClas(w, test_set, training_set) + (1 - ALPHA) * tasaRed(w);
 }
 
-fn tasa_clas(w: []const f64) f64 {
-    _ = w;
+fn tasaClas(w: []const f64, test_set: []const Example, training_set: []const Example) f64 {
+    // Classify every example in test_set using training_set and weights w
+    const leave_one_out = std.meta.eql(test_set, training_set);
 
+    var well_classified: usize = 0;
+    for (test_set, 0..) |example, i| {
+        const skip_idx = if (leave_one_out) i else null;
+        const class = classifier1NN(example, training_set, skip_idx, Example.distanceWeighted, w);
+        if (std.mem.eql(u8, class, example.class))
+            well_classified += 1;
+    }
+
+    return 100.0 * @intToFloat(f64, well_classified) / @intToFloat(f64, test_set.len);
 }
 
-fn tasa_red(weights: []const f64) f64 {
+fn tasaRed(weights: []const f64) f64 {
     var discarded: usize = 0;
     for (weights) |weight| {
         if (weight < MIN_WEIGHT) {
             discarded += 1;
         }
     }
-    return 100 * discarded / weights.len;
+    return 100.0 * @intToFloat(f64, discarded) / @intToFloat(f64, weights.len);
 }
-
 
 fn classifier1NN(e: Example, set: []const Example, skip_idx: ?usize, comptime distanceFn: anytype, ctx: anytype) []const u8 {
     var c_min: []const u8 = undefined;
     var d_min = std.math.floatMax(f64);
-    for (set) |example, i| {
+    for (set, 0..) |example, i| {
         if (skip_idx != null and skip_idx.? == i)
             continue;
         const d = distanceFn(e, example, ctx);
@@ -159,44 +160,206 @@ fn classifier1NN(e: Example, set: []const Example, skip_idx: ?usize, comptime di
     return c_min;
 }
 
+const MOV_VARIANCE = 0.3;
+const MOV_STDDEV = std.math.sqrt(MOV_VARIANCE);
+
+fn mov(w: []f64, i: usize, rnd: Random) void {
+    const z_i = rndNorm(rnd, 0, MOV_STDDEV);
+    const result = w[i] + z_i;
+    const result_truncated = std.math.max(0, std.math.min(1, result));
+    w[i] = result_truncated;
+}
+
+fn rndNorm(rnd: Random, mean: f64, stddev: f64) f64 {
+    return rnd.floatNorm(f64) * stddev + mean;
+}
+
+fn busquedaLocal(training_set: []const Example, allocator: Allocator, rnd: Random) ![]const f64 {
+    const n = training_set[0].attributes.len;
+
+    // First solution
+    const w = try allocator.alloc(f64, n);
+    errdefer allocator.free(w);
+    for (w) |*weight| {
+        weight.* = rnd.float(f64); // range 0, 1
+    }
+    var current_fitness = getFitness(w, training_set, training_set);
+
+    // Mutation of w
+    const w_mut = try allocator.alloc(f64, n);
+    defer allocator.free(w_mut);
+
+    // Indexes
+    const indexes = try allocator.alloc(usize, n);
+    defer allocator.free(indexes);
+    for (indexes, 0..) |*index, i| {
+        index.* = i;
+    }
+    rnd.shuffle(usize, indexes);
+
+    var neighbours: usize = 0;
+    var iter: usize = 0;
+    while (neighbours < 20 * n and iter < 15000) : ({
+        iter += 1;
+        neighbours += 1;
+    }) {
+        // Mutate w into w_mut
+        std.mem.copy(f64, w_mut, w);
+        mov(w_mut, indexes[iter % n], rnd);
+
+        // Evaluate w_mut, classifying every example in training_set using leave-one-out
+        const fitness = getFitness(w_mut, training_set, training_set);
+        if (fitness > current_fitness) {
+            // Replace w with w_mut
+            current_fitness = fitness;
+            std.mem.copy(f64, w, w_mut);
+            rnd.shuffle(usize, indexes);
+            neighbours = 0;
+        }
+    }
+
+    // print("iterations: {}, neighbours: {}\n", .{iter, neighbours});
+
+    return w;
+}
+
+fn abs(f: f64) f64 {
+    return if (f < 0) -f else f;
+}
+
+fn greedy(training_set: []const Example, allocator: Allocator, rnd: Random) ![]const f64 {
+    _ = rnd; // rnd is unused, since greedy is deterministic
+
+    const n = training_set[0].attributes.len;
+
+    // First solution
+    const w = try allocator.alloc(f64, n);
+    errdefer allocator.free(w);
+    std.mem.set(f64, w, 0);
+
+    for (training_set, 0..) |example, i_example| {
+        // Get the closest enemy and closest friend to `example`
+        var closest_enemy_i: usize = 0;
+        var closest_friend_i: usize = 0;
+        var closest_enemy_distance = std.math.floatMax(f64);
+        var closest_friend_distance = std.math.floatMax(f64);
+        for (training_set, 0..) |other, i| {
+            if (i == i_example) continue;
+            const dist = example.distance(other, {});
+            if (std.mem.eql(u8, example.class, other.class)) {
+                if (dist < closest_friend_distance) {
+                    closest_friend_distance = dist;
+                    closest_friend_i = i;
+                }
+            } else {
+                if (dist < closest_enemy_distance) {
+                    closest_enemy_distance = dist;
+                    closest_enemy_i = i;
+                }
+            }
+        }
+
+        // Add the difference to the closest enemy and substract the difference
+        // to the closest friend
+        const enemy_attrs = training_set[closest_enemy_i].attributes;
+        const friend_attrs = training_set[closest_friend_i].attributes;
+        for (w, example.attributes, enemy_attrs, friend_attrs) |*weight, attr, enemy_attr, friend_attr| {
+            weight.* += abs(attr - enemy_attr) - abs(attr - friend_attr);
+        }
+    }
+
+    // Normalize w
+    const w_max = std.mem.max(f64, w);
+    for (w) |*weight| {
+        weight.* = if (weight.* < 0) 0 else weight.* / w_max;
+    }
+
+    return w;
+}
+
+fn joinPartitions(partitions: [5][]const Example, skip_idx: usize, allocator: Allocator) ![]Example {
+    var used_partitions: [4][]const Example = undefined;
+    for (0..partitions.len) |i| {
+        if (i == skip_idx) continue;
+        const i_save = if (i < skip_idx) i else i - 1;
+        used_partitions[i_save] = partitions[i];
+    }
+
+    return try std.mem.concat(allocator, Example, &used_partitions);
+}
+
+fn readPartitions(comptime name: []const u8, allocator: Allocator) ![5][]Example {
+    var examples_partitions: [5][]Example = undefined;
+    inline for (&examples_partitions, 0..) |*examples, i| {
+        const path = std.fmt.comptimePrint("Instancias_APC/{s}_{}.arff", .{ name, i + 1 });
+        examples.* = try readExamplesFromFile(path, allocator);
+    }
+    return examples_partitions;
+}
+
+const RNG_SEED = 16;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    var rng = std.rand.DefaultPrng.init(0);
+    var rng = std.rand.DefaultPrng.init(RNG_SEED);
     const rnd = rng.random();
 
-    var w: [8]f64 = undefined;
-    for (w) |*weight| {
-        weight.* = rnd.float(f64);
+    const partitions = try readPartitions("diabetes", allocator);
+    defer {
+        for (partitions) |partition| {
+            for (partition) |example| {
+                example.deinit(allocator);
+            }
+            allocator.free(partition);
+        }
     }
 
-    std.debug.print("{any}\n", .{w});
+    for (0..5) |i| {
+        print("[PARTICION {}]\n", .{i+1});
+        const training_set = try joinPartitions(partitions, i, allocator);
+        defer allocator.free(training_set);
+        const test_set = partitions[i];
+        print("training: {}, test: {} \n", .{training_set.len, test_set.len});
+        for (0..5) |j| {
+            print("{d} {s}\n", .{training_set[j].attributes, training_set[j].class});
+        }
+        _ = rnd;
 
+        // const w_bl = try busquedaLocal(training_set, allocator, rnd);
+        // defer allocator.free(w_bl);
+        // const fitness_bl = getFitness(w_bl, test_set, training_set);
+        // print("BL: fitness {d}, tasa_red {d}, tasaClas {d}\n", .{
+        //     fitness_bl,
+        //     tasaRed(w_bl),
+        //     tasaClas(w_bl, test_set, training_set),
+        // });
 
-    const examples = try readExamplesFromFile("Instancias_APC/diabetes_1.arff", allocator);
-    // const examples2 = try readExamplesFromFile("Instancias_APC/diabetes_2.arff", allocator);
+        // const w_greedy = try greedy(training_set, allocator, rnd);
+        // defer allocator.free(w_greedy);
+        // const fitness_greedy = getFitness(w_greedy, test_set, training_set);
+        // print("GREEDY: fitness {d}, tasa_red {d}, tasaClas {d}\n", .{
+        //     fitness_greedy,
+        //     tasaRed(w_greedy),
+        //     tasaClas(w_greedy, test_set, training_set),
+        // });
 
+        // print("w greedy: ", .{});
+        // for (w_greedy) |weight| {
+        //     print("{d} ", .{weight});
+        // }
+        // print("\n", .{});
 
-
-    // _ = examples;
-    for (examples) |example, i| {
-        const result1 = classifier1NN(example, examples, i, Example.distanceWeighted, &w);
-        const result2 = classifier1NN(example, examples, i, Example.distance, {});
-        // _ = result1;
-        // _ = result2;
-        if (!std.mem.eql(u8, result1, example.class))
-            std.debug.print("failed1 {}\n", .{i});
-        if (!std.mem.eql(u8, result2, example.class))
-            std.debug.print("failed2 {}\n", .{i});
+        print("\n", .{});
     }
 
+    // std.mem.copy(Example, examples, examples1);
+    // std.mem.copy(Example, examples[examples1.len..], examples2);
+    // std.mem.copy(Example, examples[examples1.len + examples2.len..], examples3);
+    // std.mem.copy(Example, examples[examples1.len + examples2.len + examples3.len..], examples4);
 
-    for (examples) |example| {
-        example.deinit(allocator);
-    }
-    allocator.free(examples);
+    // _ = w;
 
 }
